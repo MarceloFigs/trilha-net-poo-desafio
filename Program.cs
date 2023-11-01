24f0c74246fa7183eba308a20962857ef0d6eb21
MarceloFigs <marcelofigueiredo.dev@gmail.com>
2023-11-01 11:15:35 -0300

    refactor: implementada logica das classes Nokia, Iphone e Smartphone.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("1399981122", "Nokia XYZ", "0123456789", 128);
+Iphone iphone = new Iphone("2188835533", "I15", "9876543210", 256);
+
+Console.WriteLine("Aparelho Nokia");
+nokia.InstalarAplicativo("Telegram");
+nokia.Ligar();
+
+Console.WriteLine();
+
+Console.WriteLine("Aparelho Iphone");
+iphone.InstalarAplicativo("WhatsApp");
+iphone.ReceberLigacao();
+
